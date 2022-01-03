a7c252a081774f73d4ba82b335539ece950d25d5
shadmanAh <shadman061@gmail.com>
2022-01-03 18:17:16 +0330

    errors

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -18,7 +18,7 @@ module RoomsHelper
         user_room = room.reserves.where(user: current_user)
         if current_user
             if room.reserves.where(user: current_user).any?
-                
+                "You Booked this room."
                 link_to "For more convenience Complete your information.", edit_reserf_path(user_room.first), class: "btn btn-md btn-danger"
             else
                 link_to "Book #{number_to_currency(room.price)} Per night ", new_room_reserf_path(room), class: "btn btn-md btn-success"
